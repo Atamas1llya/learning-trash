fc628891cb72441ba5318409afe8d640dda5d560
Atamas Illya <atamas1llya@gmail.com>
2017-09-03 15:35:34 +0300

    Add python interpretation of binary2decimal

---
+++ binary2decimal.py
@@ -0,0 +1,13 @@
+import sys
+
+def binary2decimal(binary):
+    binary = list(binary)[::-1];
+    decimal = 0;
+
+    for index, bit in enumerate(binary):
+        if int(bit):
+            decimal += 2**index;
+
+    return decimal;
+
+print(binary2decimal(sys.argv[1]));

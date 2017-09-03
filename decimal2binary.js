d1edaf81bc64c7fa2c3e7a64f26cef64c718d843
Atamas Illya <atamas1llya@gmail.com>
2017-09-03 13:11:55 +0300

    Optimization

--- decimal2binary.js
+++ decimal2binary.js
@@ -3,33 +3,33 @@ const decimalToBinary = (decimal) => {
   let rest = decimal;
 
   while (rest > 0) {
+
     if (+rest === 1) {
       bits[bits.length - 1] = '1';
       rest = 0;
-    }
+    };
 
-    for (let i = 1; Math.pow(2, i) <= decimal; i++) {
+    for (var i = 1; Math.pow(2, i) <= decimal; i++) {
       if (decimal < Math.pow(2, i + 1)) {
         bits[i] = '1';
-        for (let x = i - 1; x >= 0; x--) {
+        for (var x = i - 1; x >= 0; x--) {
           bits[x] = '0';
-        }
-
+        };
         rest = decimal - Math.pow(2, i);
-      }
-    }
+      };
+    };
 
-    for (let i = bits.length; i >= 0; i--) {
-      if (Math.pow(2, i) === decimal) {
-        bits[i] = '1';
-      }
-    }
+    for (var y = bits.length; y >= 0; y--) {
+      if (Math.pow(2, y) === decimal) {
+        bits[y] = '1';
+      };
+    };
 
     decimal = rest;
-  }
+  };
   return bits.reverse().join('');
-}
-
-module.exports = decimalToBinary;
+};
 
 console.log(decimalToBinary(+process.argv[2]));
+
+module.exports = decimalToBinary;

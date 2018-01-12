c664afb1c2b7742abd8d56292c5862850aa2bef0
Ilya Atamas <atamas1llya@gmail.com>
2018-01-12 11:50:46 +0200

    Start WoT data processing

--- py/ml/wot_vehicles_classifier/fetcher.py
+++ py/ml/wot_vehicles_classifier/fetcher.py
@@ -1,15 +1,23 @@
+
+import urllib.request
+import json
+
 wot_res = urllib.request.urlopen('https://api.worldoftanks.ru/wot/encyclopedia/vehicles/?application_id=8ca40e4d4a53687d8e5fae2b3f772674&limit=1')
 wot_data_json = wot_res.read().decode('utf-8')
 wot_data_raw = json.loads(wot_data_json)['data']
 
-required_keys = ['type', 'tier', 'engines', 'turrets']
+required_keys = ['type', 'tier', 'engines', 'turrets', 'suspensions', 'guns', 'radios', 'tank_id']
 wot_data = []
 
 for index, item in enumerate(wot_data_raw):
     wot_data.append({})
     for key in wot_data_raw[item].keys():
         if key in required_keys:
-            wot_data[index][key] = wot_data_raw[item][key]
+            if isinstance(wot_data_raw[item][key], list) and len(wot_data_raw[item][key]) > 0:
+                wot_data[index][key] = wot_data_raw[item][key][-1]
+            else:
+                wot_data[index][key] = wot_data_raw[item][key]
+
 
 file = open('./datasets/wot_raw.json', 'w')
-file.write(json.dumps(wot_data))
+file.write(json.dumps(wot_data, indent=2, sort_keys=True))
--- py/ml/wot_vehicles_classifier/processor.py
+++ py/ml/wot_vehicles_classifier/processor.py
@@ -2,7 +2,45 @@
 import urllib.request
 import json
 
+APP_ID = '8ca40e4d4a53687d8e5fae2b3f772674'
+
 raw_data = json.load(open('./datasets/wot_raw.json', 'r'))
 
+# model:
+# X: [spec_power, damage_per_shot, avg_armor, view_range]
+# Y: [type]
+
+data_x = []
+data_y = []
+
+def request_tmm(tank_id, engines, guns, suspensions, radios, turrets, **keys):
+    tmm_url = f'https://api.worldoftanks.ru/wot/encyclopedia/vehicleprofile/?application_id={APP_ID}\
+        &tank_id={tank_id}\
+        &engine_id={engines}\
+        &gun_id={guns}\
+        &suspension_id={suspensions}\
+        &radio_id={radios}'
+
+    if isinstance(turrets, int):
+        tmm_url += f'&turret_id={turrets}'
+
+    tmm_res = urllib.request.urlopen(tmm_url)
+
+    tmm_json = tmm_res.read().decode('utf-8')
+    tmm = json.loads(tmm_json)
+    if (tmm['status'] == 'ok'):
+        tank_tmm = tmm['data'][f'{tank_id}']
+        tank_x = []
+
+        tank_x.append(int(tank_tmm['engine']['power'] / tank_tmm['weight'] * 1000))
+        tank_x.append(tank_tmm['ammo'][0]['damage'][1])
+        tank_x.append(int(((tank_tmm['armor']['hull']['front'] + tank_tmm['armor']['hull']['sides']) / 2)))
+        tank_x.append(tank_tmm['turret']['view_range'])
+
+        return tank_x, keys['type']
+    else:
+        return None, keys['type']
+
 for vehicle in raw_data:
-    print(vehicle)
+    tmm, type = request_tmm(**vehicle)
+    print(tmm, type)

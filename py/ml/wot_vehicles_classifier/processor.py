9442539d3d8b78c29ff9556e0463c3d69abf09e1
Ilya Atamas <atamas1llya@gmail.com>
2018-01-12 13:29:52 +0200

    Add executable script

--- py/ml/wot_vehicles_classifier/processor.py
+++ py/ml/wot_vehicles_classifier/processor.py
@@ -6,6 +6,7 @@ import os.path
 APP_ID = '8ca40e4d4a53687d8e5fae2b3f772674'
 
 raw_data = json.load(open('./datasets/wot_raw_v2.json', 'r'))
+cache_folder = '__tmcache__'
 
 # model:
 # X: [spec_power, damage_per_shot, avg_stun, siege, avg_armor, view_range, reload_time, traverse_speed]
@@ -35,16 +36,16 @@ def request_tmm(tank_id, engines, guns, suspensions, radios, turrets, **keys):
     if isinstance(turrets, int):
         url_link += f'&turret_id={turrets}'
 
-    if os.path.isfile(f'./tmcache/{url_link}.json'):
-        file = open(f'./tmcache/{url_link}.json', 'r')
-        tmm_json = json.load(open(f'./tmcache/{url_link}.json'))
+    if os.path.isfile(f'./{cache_folder}/{url_link}.json'):
+        file = open(f'./{cache_folder}/{url_link}.json', 'r')
+        tmm_json = json.load(open(f'./{cache_folder}/{url_link}.json'))
     else:
         tmm_res = urllib.request.urlopen(f'https://api.worldoftanks.ru/wot/encyclopedia/vehicleprofile/?application_id={APP_ID}{url_link}')
         tmm_json = tmm_res.read().decode('utf-8')
 
 
-    if not os.path.isfile(f'./tmcache/{url_link}.json'):
-        file = open(f'./tmcache/{url_link}.json', 'w')
+    if not os.path.isfile(f'./{cache_folder}/{url_link}.json'):
+        file = open(f'./{cache_folder}/{url_link}.json', 'w')
         file.write(json.dumps(tmm_json))
 
     tmm = json.loads(tmm_json)
@@ -57,7 +58,7 @@ def request_tmm(tank_id, engines, guns, suspensions, radios, turrets, **keys):
         tank_x.append(find_avg_stun(tank_tmm['ammo']))
         # tank_x.append(int(bool(tank_tmm['siege'])))
         tank_x.append(int(((tank_tmm['armor']['hull']['front'] + tank_tmm['armor']['hull']['sides']))))
-        # tank_x.append(tank_tmm['turret']['view_range'])
+        tank_x.append(tank_tmm['turret']['view_range'])
         tank_x.append(tank_tmm['gun']['reload_time'])
         tank_x.append(tank_tmm['suspension']['traverse_speed'])
 
